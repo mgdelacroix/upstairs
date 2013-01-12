c14c9792276ba87718cbb035696a6beb89700e8a
Miguel de la Cruz <mgdelacroix@gmail.com>
2013-01-12 16:08:33 +0100

    Activity class defined

--- grails-app/domain/es/ctrlz/upStairs/Activity.groovy
+++ grails-app/domain/es/ctrlz/upStairs/Activity.groovy
@@ -2,6 +2,31 @@ package es.ctrlz.upStairs
 
 class Activity {
 
+	Date dateCreated
+	Purpose purpose
+	Effort effort
+	Comment comment
+	ActivityType type
+
+	static belongsTo = [user: User]
+
     static constraints = {
+		type nullable: false, validator: { type, purpose ->
+			// En función del tipo, unos campos tendrán valor y otros no
+			if (type == ActivityType.CREATE_PURPOSE || type == ActivityType.COMPLETE_PURPOSE) {
+				purpose != null && (effort == null && comment == null)
+			} else if (type == ActivityType.REGISTER_EFFORT) {
+				effort != null && (purpose == null && comment == null)
+			} else {
+				comment != null && (purpose == null && effort == null)
+			}
+		}
     }
+}
+
+enum ActivityType {
+	CREATE_PURPOSE,
+	COMPLETE_PURPOSE,
+	REGISTER_EFFORT,
+	COMMENT_EFFORT
 }
