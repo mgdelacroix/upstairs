5f029139c537ec966894a1fea0aa34178c5e01b1
Miguel de la Cruz <mgdelacroix@gmail.com>
2013-01-16 07:59:40 +0100

    First unit testgit add test/

--- test/unit/es/ctrlz/upStairs/EffortTests.groovy
+++ test/unit/es/ctrlz/upStairs/EffortTests.groovy
@@ -1,7 +1,5 @@
 package es.ctrlz.upStairs
 
-
-
 import grails.test.mixin.*
 import org.junit.*
 
@@ -9,9 +7,39 @@ import org.junit.*
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(Effort)
+@Mock([
+	Effort, Purpose
+])
 class EffortTests {
 
-    void testSomething() {
-       fail "Implement me"
+    void testConstraints() {
+		// Crear el propósito es necesario? Si es así, el usuario no?
+		def purpose = new Purpose(
+			name: 'Test purpose',
+			description: 'Description'
+		)
+		def effort = new Effort(
+			purpose: purpose,
+			description: 'Description'
+		)
+
+		/*
+		 * Ahora mismo el esfuerzo no tiene fecha, de modo que
+		 * no debería validar
+		 */
+		assert !effort.validate()
+
+		/*
+		 * El esfuerzo debería validar ahora que tiene fecha
+		 */
+		effort.date = new Date()
+		assert effort.validate()
+
+		/*
+		 * Eliminamos la descripción y comprobamos que ahora la validación
+		 * no pasa
+		 */
+		effort.description = null
+		assert !effort.validate()
     }
 }
